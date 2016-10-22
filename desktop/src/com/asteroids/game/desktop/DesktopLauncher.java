78201cd00daa35d047a3112249ddbd36235a99b5
Alexander Sychev <sandyqwe@yandex.ru>
2016-10-22 22:43:02 +0500

    Initial commit

---
+++ desktop/src/com/asteroids/game/desktop/DesktopLauncher.java
@@ -0,0 +1,14 @@
+package com.asteroids.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.asteroids.game.AsteroidsGame;
+
+public class DesktopLauncher {
+    public static void main(String[] arg) {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.height = 720;
+        config.width = 1280;
+        new LwjglApplication(new AsteroidsGame(), config);
+    }
+}
